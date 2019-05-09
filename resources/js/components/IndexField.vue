6439a43f43807c6a921816891616db9c5f33c87a
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-05-09 13:41:30 +0300

    Add basic index field value

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,9 +1,21 @@
 <template>
-    <span>{{ field.value }}</span>
+  <span>{{ value }}</span>
 </template>
 
 <script>
 export default {
-    props: ['resourceName', 'field'],
-}
+  props: ['resourceName', 'field'],
+
+  computed: {
+    value() {
+      if (!this.field.value) return '';
+      return this.field.value
+        .split(SEPARATOR)
+        .map(val => this.field.options.find(opt => String(opt.value) === val))
+        .filter(val => !!val)
+        .map(val => val.label)
+        .join(', ');
+    },
+  },
+};
 </script>
