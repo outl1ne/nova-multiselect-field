1421d60cda5bd1e8557daf4a5b8f84e9a2376f50
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-05-09 15:17:31 +0300

    Improve index field display value

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -3,18 +3,24 @@
 </template>
 
 <script>
+import SEPARATOR from '../separator';
+
 export default {
   props: ['resourceName', 'field'],
 
   computed: {
     value() {
       if (!this.field.value) return '-';
-      return this.field.value
+      const values = this.field.value
         .split(SEPARATOR)
         .map(val => this.field.options.find(opt => String(opt.value) === val))
         .filter(val => !!val)
-        .map(val => val.label)
-        .join(', ');
+        .map(val => val.label);
+
+      const joinedValues = values.join(', ');
+      if (joinedValues.length <= 40) return joinedValues;
+
+      return `${values.length} items selected`;
     },
   },
 };
