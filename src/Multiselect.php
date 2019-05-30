09f460073544933ad5ec72a6d6ec818d1cbf8ee9
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-05-30 12:29:39 +0300

    Add documentation to field configuration functions

--- src/Multiselect.php
+++ src/Multiselect.php
@@ -10,25 +10,49 @@ class Multiselect extends Field
 
     protected $pageResponseResolveCallback;
 
-    public function options($options)
+    /**
+     * Sets the options available for select.
+     *
+     * @param array $options
+     * @return \OptimistDigital\MultiselectField\Multiselect
+     **/
+    public function options($options = [])
     {
         return $this->withMeta([
-            'options' => collect($options ?? [])->map(function ($label, $value) {
+            'options' => collect($options)->map(function ($label, $value) {
                 return is_array($label) ? $label + ['value' => $value] : ['label' => $label, 'value' => $value];
             })->values()->all(),
         ]);
     }
 
+    /**
+     * Sets the max number of options the user can select.
+     *
+     * @param int $max
+     * @return \OptimistDigital\MultiselectField\Multiselect
+     **/
     public function max($max)
     {
         return $this->withMeta(['max' => $max]);
     }
 
+    /**
+     * Sets the placeholder value displayed on the field.
+     *
+     * @param string $placeholder
+     * @return \OptimistDigital\MultiselectField\Multiselect
+     **/
     public function placeholder($placeholder)
     {
         return $this->withMeta(['placeholder' => $placeholder]);
     }
 
+    /**
+     * Sets the maximum number of options displayed at once.
+     *
+     * @param int $optionsLimit
+     * @return \OptimistDigital\MultiselectField\Multiselect
+     **/
     public function optionsLimit($optionsLimit)
     {
         return $this->withMeta(['optionsLimit' => $optionsLimit]);
