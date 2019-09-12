d3d99e01222213bf0598e6a45c680e3eb2cc630d
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-09-12 10:55:44 +0300

    Add saveAsJSON option

--- src/Multiselect.php
+++ src/Multiselect.php
@@ -3,12 +3,14 @@
 namespace OptimistDigital\MultiselectField;
 
 use Laravel\Nova\Fields\Field;
+use Laravel\Nova\Http\Requests\NovaRequest;
 
 class Multiselect extends Field
 {
     public $component = 'multiselect-field';
 
     protected $pageResponseResolveCallback;
+    protected $saveAsJSON = false;
 
     /**
      * Sets the options available for select.
@@ -23,6 +25,27 @@ class Multiselect extends Field
                 return is_array($label) ? $label + ['value' => $value] : ['label' => $label, 'value' => $value];
             })->values()->all(),
         ]);
+    }
+
+    protected function fillAttributeFromRequest(NovaRequest $request, $requestAttribute, $model, $attribute)
+    {
+        if ($request->exists($requestAttribute)) {
+            $model->{$attribute} = $this->saveAsJSON === true
+                ? json_decode($request[$requestAttribute], true)
+                : $request[$requestAttribute];
+        }
+    }
+
+    /**
+     * Allows the field to save an actual JSON array to a SQL JSON column.
+     *
+     * @param bool $saveAsJSON
+     * @return \OptimistDigital\MultiselectField\Multiselect
+     **/
+    public function saveAsJSON($saveAsJSON = true)
+    {
+        $this->saveAsJSON = $saveAsJSON;
+        return $this;
     }
 
     /**
