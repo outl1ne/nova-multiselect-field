d9f10711eba487c9fb4a692b95291fa1a201368b
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-11-04 11:44:41 +0200

    Fix transpilation with Laravel Mix 5, update dist files

--- resources/js/multiselect-field.js
+++ resources/js/multiselect-field.js
@@ -1,5 +1,5 @@
 Nova.booting((Vue, router, store) => {
-  Vue.component('index-multiselect-field', require('./components/IndexField'));
-  Vue.component('detail-multiselect-field', require('./components/DetailField'));
-  Vue.component('form-multiselect-field', require('./components/FormField'));
+  Vue.component('index-multiselect-field', require('./components/IndexField').default);
+  Vue.component('detail-multiselect-field', require('./components/DetailField').default);
+  Vue.component('form-multiselect-field', require('./components/FormField').default);
 });
