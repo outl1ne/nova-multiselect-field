10562eebf8a88c9f174cd4153a17e58e73be08f0
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2020-10-22 11:46:48 +0300

    Add nova-translations-loader

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -5,7 +5,7 @@ namespace OptimistDigital\MultiselectField;
 use Laravel\Nova\Nova;
 use Laravel\Nova\Events\ServingNova;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\File;
+use OptimistDigital\NovaTranslationsLoader\NovaTranslationsLoader;
 
 class FieldServiceProvider extends ServiceProvider
 {
@@ -16,36 +16,6 @@ class FieldServiceProvider extends ServiceProvider
             Nova::style('multiselect-field', __DIR__ . '/../dist/css/multiselect-field.css');
         });
 
-        $this->translations();
-    }
-
-    protected function translations()
-    {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([__DIR__ . '/../resources/lang' => resource_path('lang/vendor/nova-multiselect-field')], 'translations');
-        } else if (method_exists('Nova', 'translations')) {
-            $locale = app()->getLocale();
-            $fallbackLocale = config('app.fallback_locale');
-
-            if ($this->attemptToLoadTranslations($locale, 'project')) return;
-            if ($this->attemptToLoadTranslations($locale, 'local')) return;
-            if ($this->attemptToLoadTranslations($fallbackLocale, 'project')) return;
-            if ($this->attemptToLoadTranslations($fallbackLocale, 'local')) return;
-            $this->attemptToLoadTranslations('en', 'local');
-        }
-    }
-
-    protected function attemptToLoadTranslations($locale, $from)
-    {
-        $filePath = $from === 'local'
-            ? __DIR__ . '/../resources/lang/' . $locale . '.json'
-            : resource_path('lang/vendor/nova-multiselect-field') . '/' . $locale . '.json';
-
-        $localeFileExists = File::exists($filePath);
-        if ($localeFileExists) {
-            Nova::translations($filePath);
-            return true;
-        }
-        return false;
+        NovaTranslationsLoader::loadTranslations(__DIR__ . '/../resources/lang', 'nova-multiselect-field', true);
     }
 }
