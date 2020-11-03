c2c6f7e28bc952613e51266628c60003ce4b4c5e
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2020-11-03 12:46:38 +0200

    Upgrade nova-translations-loader

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -5,10 +5,12 @@ namespace OptimistDigital\MultiselectField;
 use Laravel\Nova\Nova;
 use Laravel\Nova\Events\ServingNova;
 use Illuminate\Support\ServiceProvider;
-use OptimistDigital\NovaTranslationsLoader\NovaTranslationsLoader;
+use OptimistDigital\NovaTranslationsLoader\LoadsNovaTranslations;
 
 class FieldServiceProvider extends ServiceProvider
 {
+    use LoadsNovaTranslations;
+
     public function boot()
     {
         Nova::serving(function (ServingNova $event) {
@@ -16,6 +18,6 @@ class FieldServiceProvider extends ServiceProvider
             Nova::style('multiselect-field', __DIR__ . '/../dist/css/multiselect-field.css');
         });
 
-        NovaTranslationsLoader::loadTranslations(__DIR__ . '/../resources/lang', 'nova-multiselect-field', true);
+        $this->loadTranslations(__DIR__ . '/../resources/lang', 'nova-multiselect-field', true);
     }
 }
