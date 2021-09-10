30cbb22767fad1a1c5aa9d4287125d3c25b31ac8
Tim Frensch <tim@frensch.io>
2021-09-10 13:25:19 +0200

    Added the limit prop

--- src/Multiselect.php
+++ src/Multiselect.php
@@ -239,6 +239,17 @@ class Multiselect extends Field
     }
 
     /**
+     * Sets the placeholder value displayed on the field.
+     *
+     * @param string $limit
+     * @return \OptimistDigital\MultiselectField\Multiselect
+     **/
+    public function limit($limit)
+    {
+        return $this->withMeta(['limit' => $limit]);
+    }
+
+    /**
      * Sets group name for selects that need to have their values distinct.
      *
      * @param string $group
