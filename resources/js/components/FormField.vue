3d85e1f24963ee7696edfd54d3adc0ffdadf5bfc
Sébastien LeBlanc <mrsebastienleblanc@icloud.com>
2022-10-19 23:13:32 -0400

    Typo

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -18,7 +18,7 @@
           :options="currentField.apiUrl ? asyncOptions : computedOptions"
           :internal-search="!currentField.apiUrl"
           :class="errorClasses"
-          :disabled="isReadOnly"
+          :disabled="isReadonly"
           :placeholder="currentField.placeholder || currentField.name"
           :close-on-select="currentField.max === 1 || !isMultiselect"
           :multiple="isMultiselect"
