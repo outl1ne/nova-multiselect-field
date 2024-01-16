be84434ea847ef3f796026d1642f211966362a6e
Kaspar Rosin <kaspar@outl1ne.com>
2024-01-16 16:39:48 +0200

    wip: remove duskl

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Outl1ne\MultiselectField\Tests;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Orchestra\Testbench\Concerns\InteractsWithPublishedFiles;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 
@@ -10,11 +9,4 @@ class TestCase extends \Orchestra\Testbench\TestCase
 {
     use InteractsWithPublishedFiles;
     use WithWorkbench;
-
-    #[\Override]
-    protected function getBasePath()
-    {
-        // Adjust this path depending on where your override is located.
-        return __DIR__ . '/../vendor/orchestra/testbench-dusk/laravel';
-    }
 }
