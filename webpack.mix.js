d80a5fdcc62c3906a642cb9be1b617c3d52f80c3
LTKort <lars.kort94@gmail.com>
2024-09-30 14:53:52 +0200

    mixin

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,6 +7,10 @@ mix
   .setPublicPath('dist')
   .js('resources/js/entry.js', 'js')
   .vue({ version: 3 })
+  .alias({
+    'laravel-nova': path.join(__dirname, 'vendor/laravel/nova/resources/js/mixins/packages.js'),
+    'nova-mixins': path.join(__dirname, './vendor/laravel/nova/resources/js/mixins'),
+  })
   .webpackConfig({
     // stats: { children: true },
     externals: {
